95ededdbef93e8276eced3efc46c76a926041ef8
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2023-01-04 09:23:58 +0500

    add heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Building Counter by using Redux Toolkit</h1>
         <Counter />
       </header>
     </div>
